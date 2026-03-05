5c1e452760b91f7ef73bf8e8e50318161088ba2b
Jamir <jamirzmaxime@gmail.com>
2026-03-05 13:07:41 -0500

    Input box for users to add income or expense

--- frontend/app.js
+++ frontend/app.js
@@ -1,20 +1,43 @@
-
+//Button Elements
 let incomeButton = document.getElementById('add-income-btn');
 let expenseButton = document.getElementById('add-expense-btn');
+let submitButton = document.getElementById('submit-btn');
+let cancelButton = document.getElementById('cancel-btn');
+//Balance Display Element
 let balanceDisplay = document.getElementById('balance');
-
+//User Input Elements
+let inputBox = document.getElementById('input-box');
+let amountInput = document.getElementById('amount-input');
+//Balance Variable
 let balance = 0;
+let transactionType = '';
 
-incomeButton.addEventListener("click", function() {
-    let amount = prompt('Enter income amount: ')
-    amount = Number(amount);
-    balance += amount;
-    balanceDisplay.textContent = '$' + balance;
+//Button Event Listeners
+incomeButton.addEventListener('click', function() {
+    inputBox.classList.remove('hidden');
+    transactionType = 'income';
 });
 
-expenseButton.addEventListener("click", function(){ 
-    let amount = prompt('Enter expense amount: ')
-    amount = Number(amount);
-    balance -= amount;
+expenseButton.addEventListener('click', function(){ 
+    inputBox.classList.remove('hidden');
+    transactionType = 'expense';
+});
+
+submitButton.addEventListener('click', function() {
+    let amount = Number(amountInput.value);
+    if (transactionType === 'income') {
+        balance += amount;
+    } else {
+        balance -= amount;
+    }
     balanceDisplay.textContent = '$' + balance;
+    amountInput.value = '';
+    inputBox.classList.add('hidden');
+    transactionType = '';
+});
+
+cancelButton.addEventListener('click', function() {
+    amountInput.value = '';
+    inputBox.classList.add('hidden');
+    transactionType = '';
 });
